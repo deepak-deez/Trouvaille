527ee6f46ddac6e56125b141b7088f942b7b88c5
joyrudra-itobuz <joyrudra@itobuz.com>
2023-04-21 13:24:00 +0530

    TROUV-14 Feat.: Added the header Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Router from "./Routes";
 import Navbar from "./components/navbar/Navbar";
+import Header from "./pages/header/Header";
 
 const App = () => {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Header />
+    </>
+  );
 };
 
 export default App;
---
+++ src/pages/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.scss";
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <h1>Choose your exotic holidays</h1>
+      </header>
+    </>
+  );
+}
