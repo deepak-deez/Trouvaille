d00d6d6e3c6491da90ea94d3195da121bfbec98a
joyrudra-itobuz <joyrudra@itobuz.com>
2023-04-24 15:08:18 +0530

    TROUV-15 Feat: Added the Filter result section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Router from "./Routes";
+<<<<<<< HEAD
 import Navbar from "./components/navbar/Child";
+=======
+import Navbar from "./components/navbar/Navbar";
+import TripCategory from "./components/trip-category/Child";
+>>>>>>> 04d2305 (TROUV-15 Feat: Added the Filter result section)
 
 const App = () => {
   return <Navbar />;
