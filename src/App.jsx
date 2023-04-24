91363bede8f285a0c1baf54a625782f42769eaa4
joyrudra-itobuz <joyrudra@itobuz.com>
2023-04-24 17:46:10 +0530

    MAIN Core : Removed all the Mistaken Merges

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
 import React from "react";
-import Router from "./Routes";
-import Footer from "./components/footer/Child";
-import Navbar from "./components/navbar/Child";
-import TripCategory from "./components/trip-category/Child";
-
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <TripCategory />
-      <Footer />;
+      <h1>Hello World</h1>
     </>
   );
 };
