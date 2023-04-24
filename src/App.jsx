e710e04745a832c7c24bda54ca680ceecc982f19
joyrudra-itobuz <joyrudra@itobuz.com>
2023-04-24 17:42:49 +0530

    TROUV-18 Core : Deleted the Unnecessary Merges

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,20 @@ import Navbar from "./components/navbar/Navbar";
 import Navbar from "./components/navbar/Child";
 >>>>>>> 3d0afc3 (TROUV-15 Core : File name changes)
 import TripCategory from "./components/trip-category/Child";
+<<<<<<< HEAD
 >>>>>>> 04d2305 (TROUV-15 Feat: Added the Filter result section)
 
 const App = () => {
   return <Navbar />;
+=======
+
+const App = () => {
+  return (
+    <>
+      <Footer />;
+    </>
+  );
+>>>>>>> 456b2b3 (TROUV-18 Core : Deleted the Unnecessary Merges)
 };
 
 export default App;
