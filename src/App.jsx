16092388de06f8747a54e858b07416c94add088b
joyrudra-itobuz <joyrudra@itobuz.com>
2023-04-24 16:39:28 +0530

    TROUV-15 Core : File name changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Router from "./Routes";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import Navbar from "./components/navbar/Child";
 =======
 import Navbar from "./components/navbar/Navbar";
+=======
+import Navbar from "./components/navbar/Child";
+>>>>>>> 3d0afc3 (TROUV-15 Core : File name changes)
 import TripCategory from "./components/trip-category/Child";
 >>>>>>> 04d2305 (TROUV-15 Feat: Added the Filter result section)
 
