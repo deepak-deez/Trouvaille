1708ddcc6924eaf0492718881d8456321295991f
joyrudra-itobuz <joyrudra@itobuz.com>
2023-04-25 13:28:22 +0530

    TROUV-24 Feat : Added Footer and BG image

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Router from "./Routes";
+import Navbar from "./components/navbar/Child";
 import Footer from "./components/footer/Child";
 
 const App = () => {
-  return <Footer />;
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
