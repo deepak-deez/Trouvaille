25680486458d6b68c4ef91e1719ef47c6d068390
joyrudra-itobuz <joyrudra@itobuz.com>
2023-04-25 12:39:45 +0530

    TROUV-20 Fix : Added the trips contents as a page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Router from "./Routes";
 import SearchResult from "./pages/searchResult/Child";
+import Trips from "./pages/trips/Child";
 const App = () => {
   return (
     <>
-      <SearchResult />
+      <Trips />
     </>
   );
 };
