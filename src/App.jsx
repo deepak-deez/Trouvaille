ec6dd1fd17a5df5caab8416a8ae63d4fc5eccb9e
joyrudra-itobuz <joyrudra@itobuz.com>
2023-04-25 13:15:03 +0530

    TROUV-24 Fix : Changes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Router from "./Routes";
-import Navbar from "./components/navbar/Child";
+import Footer from "./components/footer/Child";
 
 const App = () => {
-  return <Navbar />;
+  return <Footer />;
 };
 
 export default App;
