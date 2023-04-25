f5e2933cc497d55a32dd876d205b31d797f7589b
zenia banerjee <zenia@itobuz.com>
2023-04-24 17:24:06 -0700

    TROUV-5 Feat. : Login form component added

---
+++ src/components/landingPage/loginForm/login.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./login.scss";
+import eye from "../../../assets/images/loginForm/eye.svg"
+
+const Header = () => {
+  return (
+    <section className="flex flex-col login-form justify-center items-center">
+      <p className="md:text-[34px]">Signin</p>
+      <h2 className="md:text-[64px] text-center mt-[10px] lg:mt-[30px] text-[50px]">Welcome to Trouvaille</h2>
+      <div className="flex flex-col lg:w-[975px] w-[90%] md:px-[30px] md:py-[30px] mt-[15px] login-details px-[25px] py-[15px] lg:py-[67px] lg:px-[97px] justify-center">
+          <div className=" input-fields lg:px-[39px] px-[15px] py-[20px] lg:py-[32px] mt-[9px] ">
+            <input
+              className="bg-transparent"
+              type="text"
+              placeholder="Enter Email ID"
+            />
+          </div>
+          <div className=" input-fields lg:px-[39px] lg:py-[32px] px-[15px] py-[20px] lg:mt-[60px] mt-[30px] flex flex-row items-center justify-between">
+            <input
+              className="bg-transparent"
+              type="text"
+              placeholder="Password"
+            />
+            <button type="button">
+              <img className="input-icon" src={eye} alt="view-icon" />
+            </button>
+          </div>
+          <div className="flex flex-row mt-[20px] lg:mt-[26px] text-[14px] gap-[11px]">
+            <input type="checkbox" name="remember-me" value="yes" />
+            <p className=" grey-text">Remember Me</p>
+          </div>
+          <button className="lg:mt-[27px] mt-[20px] px-[15px] py-[20px] lg:py-[24px] text-center continue-button">
+            CONTINUE
+          </button>
+          <p className="text-center grey-text mt-[10px] lg:mt-[30px]">Forget Password</p>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
--- src/pages/signIn/signin.jsx
+++ src/pages/signIn/signin.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./signin.scss";
 import NavBar from "../../components/landingPage/navBar/navbar.jsx";
+import Signin from "../../components/landingPage/loginForm/login.jsx"
 
 const signin = () => {
   return (
         <header className="sign-in-page">
           <NavBar />
+          <Signin />
         </header>
   );
 };
