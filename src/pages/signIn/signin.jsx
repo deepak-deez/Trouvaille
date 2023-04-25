8b3c70d20f5663a9bcffc8e79ca59cb7d7ea09f1
zenia banerjee <zenia@itobuz.com>
2023-04-24 18:31:49 -0700

    TROUV-5 Feat. : Footer Component added

--- src/pages/signIn/signin.jsx
+++ src/pages/signIn/signin.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./signin.scss";
 import NavBar from "../../components/landingPage/navBar/navbar.jsx";
 import Signin from "../../components/landingPage/loginForm/login.jsx"
-
+import Footer from "../../components/landingPage/footer/footer.jsx"
 const signin = () => {
   return (
         <header className="sign-in-page">
           <NavBar />
           <Signin />
+          <Footer />
         </header>
   );
 };
