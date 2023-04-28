b5441518e10a4620fff99c319af0d78111fb38d5
joyrudra-itobuz <joyrudra@itobuz.com>
2023-04-28 17:09:45 +0530

    TROUV-11 Fix : Recovered old files

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,19 +1,10 @@
-
 import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Signin from "./pages/signIn/Child.jsx";
-import SetPassword from "./pages/setPassword/Child.jsx";
-import ResetPassword from "./pages/resetPassword/Child.jsx"
-import Signup from "./pages/signUp/Child.jsx"
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                    <Route path="/" element={<Signin/>} exact />
-                    <Route path="/setPassword"  element={<SetPassword/>} />
-                    <Route path="/resetPassword"  element={<ResetPassword/>} />
-                    <Route path="/signup"  element={<Signup/>} />
             </Routes>
         </BrowserRouter>
     );
