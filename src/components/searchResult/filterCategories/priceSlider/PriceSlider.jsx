0ecef2ad36237ab3a6acacaa3fae78e7a6c4912d
zenia banerjee <zenia@itobuz.com>
2023-05-24 18:06:14 +0530

    TROUV-112 FEAT. : Dynamically added the max and min value in the slider

--- src/components/searchResult/filterCategories/priceSlider/PriceSlider.jsx
+++ src/components/searchResult/filterCategories/priceSlider/PriceSlider.jsx
@@ -1,28 +1,61 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Slider from "rc-slider";
+import getAllApiData from "./logic";
 
 export default function PriceSlider() {
   const [filterSliderValue, setFilterSliderValue] = useState(0);
+  let allPackagesData = useRef();
+  let maxPrice = useRef();
+  let minPrice = useRef();
+
+  const [max, setMax] = useState();
+  const [min, setMin] = useState();
+  const tripPrice = [];
+
+  useEffect(() => {
+    priceDataArray();
+  }, []);
+
+  const priceDataArray = async () => {
+    allPackagesData = await getAllApiData().then((res) => {
+      return res;
+    });
+
+    allPackagesData?.map((data, index) => {
+      tripPrice.push(data.price);
+    });
+    maxPrice = 0;
+    minPrice = tripPrice[0];
+    tripPrice.map((data, index) => {
+      if (data > maxPrice) {
+        maxPrice = data;
+      }
+      if (data < minPrice) {
+        minPrice = data;
+      }
+    });
+    setMax(maxPrice);
+    setMin(minPrice);
+  };
 
   const onChangeEventTriggered = (newValue) => {
     console.log(`${newValue}`);
     setFilterSliderValue(newValue);
   };
-
   return (
     <div className="flex flex-col gap-5 lg:w-1/4 xl:w-full">
       <h4>Price</h4>
       <Slider
         value={filterSliderValue}
-        step={1}
-        min={0}
-        max={22500}
+        step={1000}
+        min={min}
+        max={max}
         onChange={onChangeEventTriggered}
       />
       <div className="flex justify-between">
-        <span>{filterSliderValue}</span>
-        <span>22500</span>
+        <span>{min}</span>
+        <span>{max}</span>
       </div>
     </div>
   );
