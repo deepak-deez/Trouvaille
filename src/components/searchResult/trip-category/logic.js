b7b6b6c6518c41705b00f3698622ffe83f1c9582
joyrudra-itobuz <joyrudra@itobuz.com>
2023-05-25 15:41:08 +0530

    TROUV-115 Fix : Changed the fetching methods to get again(Only Filter methods will be changed to post)

--- src/components/searchResult/trip-category/logic.js
+++ src/components/searchResult/trip-category/logic.js
@@ -3,9 +3,7 @@ import axios from "axios";
 const allTripPackagesApi = `${process.env.REACT_APP_API_HOST}get-module/trip-package`;
 
 const getAllApiData = async (setAllTripData) => {
-  const getAllTripPackagesData = await axios.post(allTripPackagesApi, {
-    category: {},
-  });
+  const getAllTripPackagesData = await axios.get(allTripPackagesApi);
   setAllTripData(getAllTripPackagesData.data.data);
 };
 
