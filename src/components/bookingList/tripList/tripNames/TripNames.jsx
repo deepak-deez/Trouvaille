5415cbb5f37314266979efbfec8ee249211f98e1
zenia banerjee <zenia@itobuz.com>
2023-05-26 17:56:05 +0530

    TROUV-116 FEAT. : Now showing user Booking details from backend

--- src/components/bookingList/tripList/tripNames/TripNames.jsx
+++ src/components/bookingList/tripList/tripNames/TripNames.jsx
@@ -1,24 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import "./style.scss";
-
+import axios from "axios";
 export default function TripNames(props) {
+  const navigate = useNavigate();
+  const tripId = props.tripId;
+  const [duration, setDuration] = useState();
+  const [price, setPrice] = useState();
+
+  const getTripData = async () => {
+    const tripDetailsUrl = `${process.env.REACT_APP_API_HOST}get-trip-details/trip-package/${tripId}`;
+    const getTripDataResponse = await axios.get(tripDetailsUrl);
+    setDuration(getTripDataResponse.data.data[0].duration);
+    setPrice(getTripDataResponse.data.data[0].price);
+  };
+
+  useEffect(() => {
+    getTripData();
+  }, []);
+
   return (
     <tr className="bg-white grey-text ">
       <td className="rounded-tl-xl rounded-bl-xl  py-[15px] lg:px-[40px] px-[10px]">
         {props.title}
       </td>
-      <td className="  py-[15px] lg:px-[40px] px-[10px]">{props.duration}</td>
+      <td className="  py-[15px] lg:px-[40px] px-[10px]">{duration}</td>
       <td className="  py-[15px] lg:px-[40px] px-[10px]">{props.passengers}</td>
-      <td className="  py-[15px] lg:px-[40px] px-[10px]">{props.price}</td>
+      <td className="  py-[15px] lg:px-[40px] px-[10px]">{price}</td>
       <td className="  py-[15px] lg:px-[40px] px-[10px]">{props.status}</td>
       <td className="rounded-tr-xl rounded-br-xl   py-[15px] lg:px-[40px] px-[10px]">
-        <Link
-          to="/bookingDetails"
+        <button
+          navigate="/bookingDetails"
           className="px-[25px] py-[10px] view-btn text-[white]"
         >
           View
-        </Link>
+        </button>
       </td>
     </tr>
   );
