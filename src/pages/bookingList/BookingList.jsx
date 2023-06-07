bf9968f6668e5e2ecd22aa3dfb0990bee0849b8f
Deepak Patra <deepak@itobuz.com>
2023-06-07 11:05:46 +0530

    TROUV-127-User-Page-Changes FIX: Component name

--- src/pages/bookingList/BookingList.jsx
+++ src/pages/bookingList/BookingList.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./style.scss";
-import BookingsList from "../../components/bookingList/tripList/TripList";
+import TripList from "../../components/bookingList/tripList/TripList";
 
 const BookingList = () => {
   return (
     <header className="trip-list-page pt-[10rem] pb-[35rem] sm:pb-[20rem]">
-      <BookingsList />
+      <TripList />
     </header>
   );
 };
