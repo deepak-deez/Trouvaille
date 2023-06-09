b310722571f812d0923b26249837f9e6edf3e435
zenia banerjee <zenia@itobuz.com>
2023-06-09 14:49:37 +0530

    TROUV-154 FIX : Now different components are getting rendered instead of the whole page the account and profile sections

--- src/pages/viewAccountDetails/ViewAccountDetails.jsx
+++ src/pages/viewAccountDetails/ViewAccountDetails.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
-import Header from "../../components/accountDetails/viewAccountDetails/ViewAccountDetails";
+import ViewAccountDetails from "../../components/accountDetails/viewAccountDetails/ViewAccountDetails";
+import EditAccountDetails from "../../components/accountDetails/editAccountDetails/EditAccountDetails";
+import ProfileSettings from "../../components/accountDetails/profileSettings/ProfileSettings";
+import Editprofile from "../../components/accountDetails/editProfile/Editprofile";
 
 const ViewAccDetails = () => {
+  const [active, setActive] = useState("view-account");
+
   return (
     <section className="account-details pt-[10rem] pb-[35rem] sm:pb-[20rem]">
-      <Header />
+      {active === "view-account" && (
+        <ViewAccountDetails
+          setActive={setActive}
+          title="view-account-details"
+        />
+      )}
+      {active === "edit-account" && (
+        <EditAccountDetails
+          setActive={setActive}
+          title="edit-account-details"
+        />
+      )}
+      {active === "profile" && (
+        <ProfileSettings setActive={setActive} title="profile-settings" />
+      )}
+      {active === "edit-profile" && (
+        <Editprofile setActive={setActive} title="edit-profile" />
+      )}
     </section>
   );
 };
