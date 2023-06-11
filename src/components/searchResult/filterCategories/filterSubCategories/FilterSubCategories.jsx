692dcab66b87480d929c153babc6b51ca3fcfe93
Joyrudra Biswas <joyrudra@itobuz.com>
2023-06-11 12:37:54 +0530

    TROUV-150 Fix : Updated the styles of the filter checkboxes and adjusted the filter dropdown state to prevent filter resetting after toggling the dropdown

--- src/components/searchResult/filterCategories/filterSubCategories/FilterSubCategories.jsx
+++ src/components/searchResult/filterCategories/filterSubCategories/FilterSubCategories.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import dropdownIcon from "../../../../assets/images/searchResult/tripCategory/drop-drown-icon.svg";
+import Checkbox from "@mui/material/Checkbox";
+import "./style.scss";
 
 export default function FilterSubCategories({
   title,
@@ -39,33 +41,39 @@ export default function FilterSubCategories({
     <div className="lg:w-[12rem] xl:w-[auto]">
       <div className="flex justify-between ">
         <h4>{title}</h4>
-        <button className={filterToggle ? " rotate-180 " : ""}>
-          <img
-            src={dropdownIcon}
-            onClick={() => {
-              setFilterToggle(!filterToggle);
-            }}
-            alt="dropdownIcon"
-          />
+        <button
+          className={filterToggle ? " rotate-180 " : ""}
+          onClick={() => {
+            setFilterToggle(!filterToggle);
+          }}
+        >
+          <img src={dropdownIcon} alt="dropdownIcon" />
         </button>
       </div>
-      {filterToggle && (
-        <ul className="flex flex-col gap-5 xl:gap-[1.8rem] xl:text-[20px] mt-[1rem]">
-          {data.map((data, index) => {
-            return (
-              <li className="flex justify-between" key={index}>
-                <input
-                  type="checkbox"
-                  id={"filter_" + data.title}
-                  data-filter-name={data.title}
-                  onChange={handleCheckboxChange}
-                />
-                <label htmlFor={"filter_" + data.title}>{data.title}</label>
-              </li>
-            );
-          })}
-        </ul>
-      )}
+
+      <ul
+        className={
+          "flex flex-col gap-5 xl:gap-[1.8rem] xl:text-[20px] mt-[1rem] " +
+          (!filterToggle ? "hidden" : "")
+        }
+      >
+        {data?.map((data, index) => {
+          return (
+            <li className="flex gap-5 " key={index}>
+              <Checkbox
+                type="checkbox"
+                className="filter-checkboxes bg-transparent border border-white text-white"
+                id={"filter_" + data.title}
+                data-filter-name={data.title}
+                onChange={handleCheckboxChange}
+              />
+              <label className="my-auto" htmlFor={"filter_" + data.title}>
+                {data.title}
+              </label>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
