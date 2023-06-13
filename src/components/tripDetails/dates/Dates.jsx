6f4c9a9cab8145f85894954594008ea24a481752
zenia banerjee <zenia@itobuz.com>
2023-06-13 16:47:30 +0530

    TROUV-162 WIP : Activity displaying with respective dates

--- src/components/tripDetails/dates/Dates.jsx
+++ src/components/tripDetails/dates/Dates.jsx
@@ -1,25 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./style.scss";
 import swal from "sweetalert2";
 
 export default function Dates(props) {
+  const date = props.day.split(" ");
   const [toShow, setToShow] = useState(false);
+  const refOne = useRef(null);
+
+  useEffect(() => {
+    document.addEventListener("click", hideOnClickOutside, true);
+  }, []);
+
+  const detailsClickHandler = () => {
+    setToShow(!toShow);
+    props.setToShowDetails(props.detail);
+    props.setDetails(!props.details);
+  };
+
+  const hideOnClickOutside = (e) => {
+    if (refOne.current && !refOne.current.contains(e.target)) {
+      setToShow(false);
+      props.setDetails(false);
+    }
+  };
+
   return (
     <div>
-      {/* {toShow && <p>{props.detail}</p>} */}
-      <li
-        className="flex flex-col items-center px-10 py-4"
-        onClick={() => setToShow(true)}
-        onMouseLeave={() => setToShow(false)}
+      <div
+        ref={refOne}
+        className={
+          "flex flex-col date items-center px-10 py-4 " +
+          (toShow ? " dates-active " : " dates ")
+        }
+        onClick={detailsClickHandler}
       >
-        <h2 className="">{props.day}</h2>
-        <p className="">{props.month}</p>
-      </li>
-      {toShow && (
-        <div className="h-40 w-64 bg-white text-orange-500 absolute mt-4 mb-3 py-3 text-center rounded-md">
+        <h2 className="">{date[0]}</h2>
+        <p className="">{date[1]}</p>
+      </div>
+      {/* {toShow && (
+        <span className=" activities text-[1rem] md:text-[2rem] mt-[2rem] ml-[-4rem] font-extrabold  p-5 absolute rounded-2xl">
           {props.detail}
-        </div>
-      )}
+        </span>
+      )} */}
     </div>
   );
 }
