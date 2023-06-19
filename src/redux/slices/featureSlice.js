9b36a1bc3ca48f800253ecb91cfaf30ea3abf4cc
rahulkarmakaritobuz <rahul.karmakar@itobuz.com>
2023-06-20 00:21:44 +0530

    TROUV-174 FEAT : Slice created for trip booking, feature, and package

---
+++ src/redux/slices/featureSlice.js
@@ -0,0 +1,91 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const API = process.env.REACT_APP_API_HOST;
+
+const nameSpace = "feature";
+
+const initialState = {
+  featureData: null,
+  loading: false,
+  error: null,
+  success: false,
+};
+
+export const getFeature = createAsyncThunk(
+  `${nameSpace}/getFeature`,
+  async (featureName, { rejectWithValue }) => {
+    try {
+      const result = await axios.get(`${API}get-feature`, featureName);
+      if (result) return result.data;
+    } catch (err) {
+      return rejectWithValue(err.response.data.message);
+    }
+  }
+);
+
+export const getFilteredFeature = createAsyncThunk(
+  `${nameSpace}/getFilteredFeature`,
+  async ({ rejectWithValue }) => {
+    try {
+      const result = await axios.get(
+        `${API}get-filtered-feature/trip-package`,
+        featureName
+      );
+      if (result) return result.data;
+    } catch (err) {
+      return rejectWithValue(err.response.data.message);
+    }
+  }
+);
+
+const featureSlice = createSlice({
+  name: nameSpace,
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(getFeature.pending, (state, action) => {
+      state.featureData = null;
+      state.loading = true;
+      state.error = null;
+      state.success = false;
+    });
+    builder.addCase(getFeature.fulfilled, (state, action) => {
+      state.featureData = action.payload.data.message;
+      state.loading = false;
+      state.error = null;
+      state.success = true;
+    });
+    builder.addCase(getFeature.rejected, (state, action) => {
+      state.featureData = null;
+      state.loading = false;
+      state.error = action.payload;
+      state.success = false;
+    });
+
+    builder.addCase(getFilteredFeature.pending, (state, action) => {
+      state.featureData = null;
+      state.loading = true;
+      state.error = null;
+      state.success = false;
+    });
+    builder.addCase(getFilteredFeature.fulfilled, (state, action) => {
+      state.featureData = action.payload.data.message;
+      state.loading = false;
+      state.error = null;
+      state.success = true;
+    });
+    builder.addCase(getFilteredFeature.rejected, (state, action) => {
+      state.featureData = null;
+      state.loading = false;
+      state.error = action.payload;
+      state.success = false;
+    });
+  },
+});
+
+export default featureSlice.reducer;
+// get-filtered-feature/trip-package
+// get-feature/category
+// get-feature/amenity
+// get-feature/occasion
