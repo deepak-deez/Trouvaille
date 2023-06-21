5b0ead2a954c5324b10bbe27bb7495d36e00943b
joyrudra-itobuz <joyrudra@itobuz.com>
2023-06-21 13:17:03 +0530

    TROUV-193 : FEAT. : Now Showing Updates as Notifications in realtime

--- src/components/viewNotifications/notificationPopUp/NotificationPopUp.jsx
+++ src/components/viewNotifications/notificationPopUp/NotificationPopUp.jsx
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { notifications } from "../data";
 import { Link } from "react-router-dom";
 import "./style.scss";
 
-export default function NotificationPopUp() {
+export default function NotificationPopUp({ statusNotis }) {
+  console.log(statusNotis);
+
+  // useEffect(() => {
+  //   socket.on("getNotis", (data) => {
+  //     console.log("You have a new Notification : ", data);
+  //   });
+  // }, [socket]);
+
+  // useEffect(() => {
+  //   socket.on("hello-bye", (data) => {
+  //     console.log(data);
+  //   });
+  // }, [socket]);
+
+  // useEffect(() => {
+  //   socket.emit("getId", userId);
+  //   socket.on("response", (data) => {
+  //     console.log(data);
+  //   });
+  // }, []);
+
   return (
     <div className="notification-popup absolute bottom-[-1rem] right-0 top-[110%] w-[25rem]  h-[25rem] overflow-auto bg-white p-4 rounded-3xl">
-      {notifications?.map((data, index) => {
-        console.log(data, index);
+      {statusNotis?.map((data, index) => {
         return (
           <div
             key={index}
             className="notification-popup-item p-3 border border-orange-700 my-2 rounded-3xl"
           >
             <h2 className="font-bold">{data.title}</h2>
-            <p className="my-3">{data.message}</p>
-            <p className="text-right text-xs text-gray-400">{data.date}</p>
+            <p className="my-3">{data.description}</p>
+            <p className="text-right text-xs text-gray-400">{data.createdAt}</p>
             <Link
-              to={"/notifications"}
+              to={"/bookingDetails/" + data.refId}
               className=" text-center text-xs p-2 bg-orange-700 text-white rounded-3xl"
             >
               View Details
